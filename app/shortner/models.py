454889361d5d9ca1c8037a3707d83660e6772336
Kia Hamedi <kia.arta9793@gmail.com>
2023-10-18 01:23:55 +0330

    add url model

--- app/shortner/models.py
+++ app/shortner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    ip = models.CharField(max_length=200, verbose_name="IP")
+    link = models.CharField(max_length=10000, verbose_name="Link")
+    uuid = models.CharField(max_length=10, verbose_name="UUID")
+    expire = models.DateTimeField(null=True, blank=True, verbose_name="Expire")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.link
