d260455308e347179e26105aad84da31f0f242ec
Kia Hamedi <kia.arta9793@gmail.com>
2023-10-18 02:41:01 +0330

    add buttons for copy

--- app/static/shortner/js/main.js
+++ app/static/shortner/js/main.js
@@ -1,5 +1,16 @@
 console.log("Can you find me?");
 
+
+$(document).on('click', '#btn-link-one', function(){ 
+    navigator.clipboard.writeText($("#link-one").text());
+    $("#btn-link-one").text("Copied");
+});
+
+$(document).on('click', '#btn-link-two', function(){ 
+    navigator.clipboard.writeText($("#link-two").text());
+    $("#btn-link-one").text("Copied");
+});
+
 $(document).on('submit', '#post-form', function(e){
     e.preventDefault();
 
@@ -12,9 +23,10 @@ $(document).on('submit', '#post-form', function(e){
         },
         success: function(data){
             $("#short-one").html('');
-            $("#short-one").append('URL Short 1: <a href="http://127.0.0.1:8000/' +data+ '">http://127.0.0.1:8000/' + data + '</a>');
+            $("#short-one").append('URL Short 1: <a id="link-one" href="http://127.0.0.1:8000/' +data+ '">http://127.0.0.1:8000/' + data + '</a> <button id="btn-link-one">Copy</button>');
             $("#short-two").html('');
-            $("#short-two").append('URL Short 2: <a href="https://short.kiahamedi.ir/' +data+ '">https://short.kiahamedi.ir/' + data + '</a>');
+            $("#short-two").append('URL Short 2: <a id="link-two" href="https://short.kiahamedi.ir/' +data+ '">https://short.kiahamedi.ir/' + data + '</a> <button id="btn-link-two">Copy</button>');
         }
     });
 });
+
