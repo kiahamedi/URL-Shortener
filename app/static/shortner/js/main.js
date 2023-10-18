490f47c60420e332ee06dcb656ccd5ee1c935998
Kia Hamedi <kia.arta9793@gmail.com>
2023-10-18 20:30:57 +0330

    add qr

--- app/static/shortner/js/main.js
+++ app/static/shortner/js/main.js
@@ -11,6 +11,16 @@ $(document).on('click', '#btn-link-two', function(){
     $("#btn-link-two").text("Copied");
 });
 
+$(document).on('click', '#btn-download-qr', function(){ 
+    const qrdata = $('#qrcode').children('img').attr('src');
+    console.log(qrdata)
+    const anchor = document.createElement("a");
+    anchor.href = qrdata;
+    anchor.download = "QR.png";
+    document.body.appendChild(anchor);
+    anchor.click();
+});
+
 $(document).on('submit', '#post-form', function(e){
     e.preventDefault();
 
@@ -26,6 +36,19 @@ $(document).on('submit', '#post-form', function(e){
             $("#short-one").append('URL 1: <a target="_blank" id="link-one" href="https://kiay.ir/' +data+ '">https://kiay.ir/' + data + '</a> <button id="btn-link-one">Copy</button>');
             $("#short-two").html('');
             $("#short-two").append('URL 2: <a target="_blank" id="link-two" href="https://short.kiahamedi.ir/' +data+ '">https://short.kiahamedi.ir/' + data + '</a> <button id="btn-link-two">Copy</button>');
+
+            $("#short-qr").html('');
+            $("#short-qr").append('<div id="qrcode"></div>');
+            const qrcode = new QRCode(document.getElementById('qrcode'), {
+                text: 'https://kiay.ir/' + data,
+                width: 128,
+                height: 128,
+                colorDark : '#000',
+                colorLight : '#fff',
+                correctLevel : QRCode.CorrectLevel.H
+              });
+            $("#short-qr").append('<button class="downloadqr" id="btn-download-qr">Download QR</button></a>');
+            
         }
     });
 });
