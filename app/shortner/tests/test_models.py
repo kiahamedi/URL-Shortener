09353f5babc885a680fe8e8e226c7a02ec72c8dd
Kia Hamedi <kia.arta9793@gmail.com>
2023-10-20 23:50:37 +0330

    add test model

--- app/shortner/tests/test_models.py
+++ app/shortner/tests/test_models.py
@@ -1,1 +1,16 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from shortner.models import Url
+
+
+class TestModels (TestCase):
+
+    def setUp(self):
+        self.link = Url.objects.create(
+            ip = "127.0.0.1",
+            link = "https://kiahamedi.ir",
+            uuid = "8Xs21",
+            is_expire = False,
+        )
+    
+    def test_link_creation(self):
+        self.assertEquals(self.link.uuid, "8Xs21")
